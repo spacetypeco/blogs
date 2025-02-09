195a217ce42ed4c82b28d008b95c918736876f97
Kevin Yeh <kevin.yeh@maryland.gov>
2025-02-08 23:01:57 -0500

    Fix videos

--- lib/notion_client/mapVideoUrl.tsx
+++ lib/notion_client/mapVideoUrl.tsx
@@ -7,6 +7,14 @@ export default function mapVideoUrl(block: Block) {
 
   const url = block.properties.source[0][0];
 
+  if (url.startsWith("attachment")) {
+    const parts = /^attachment:([a-z0-9\-]*):(.*)/.exec(url);
+    const newUrl = `https://prod-files-secure.s3.us-west-2.amazonaws.com/${block.space_id}/${parts[1]}/${parts[2]}`;
+    const encodedUrl = encodeURIComponent(newUrl);
+    block.properties.source[0][0] = `https://notion.so/signed/${encodedUrl}?table=block&id=${block.id}&spaceId=${block.space_id}`; //&name=${videoFilename}&userId=${userId}&cache=v2`;
+    return;
+  }
+
   if (!url.includes("prod-files-secure") || url.includes("signed")) {
     return;
   }
